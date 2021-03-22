22b583b9e712e54ca01539a67488c36d7d3e95bc
diego.taub <diego.taub@accenture.com>
2021-03-21 21:09:10 -0300

    Ejercicio15

---
+++ Ejercicio15/Ejercicio15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un valor: ");
+            if (decimal.TryParse(Console.ReadLine(), out decimal valor)){
+                Console.Write("El valor ingresado puede ser interpretado como número");
+            }
+            else
+            {
+                Console.Write("El valor ingresado no puede ser interpretado como número");
+            }
+            Console.ReadKey();
+        }
+    }
+}
